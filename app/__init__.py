6628433a03759d116450def6c5c6639215ba6682
Sharon Zhou <sharonzhou@Sharons-MacBook-Pro-4.local>
2019-08-19 13:41:05 -0700

    fixed/static secret

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ import secrets
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['SECRET_KEY']= secrets.token_urlsafe(16)
+app.config['SECRET_KEY'] = 'sharonybaloney' # secrets.token_urlsafe(16)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["DATABASE_URL"]
 db = SQLAlchemy(app)
 
